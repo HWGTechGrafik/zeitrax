c6d56061a4b029c2107196af783736c68e2e3ddd
Florian Moser <florian_moser@gmx.at>
2026-08-18 16:20:05 +0200

    Zeitrax PWA v1.8.1
    Startfehler werden im Klartext gemeldet statt endlos zu laden (Zeitlimit
    beim Start), Fehlerprotokoll ueber Ueber Zeitrax abrufbar, Uebersichts-
    karten kompakter und zum Wegklappen.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3066554810" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4046248851" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
